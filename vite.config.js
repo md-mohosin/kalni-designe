80965c13f36056a94e51e5d58ff47d7401760f7f
md mohosin <mdmohosin.web@gmail.com>
2026-01-19 12:46:40 +0600

    create product details page

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        single: resolve(__dirname, 'single.html'), // Example of a file in a subdirectory
+        single: resolve(__dirname, 'productDetails.html'), // Example of a file in a subdirectory
       },
     },
   },

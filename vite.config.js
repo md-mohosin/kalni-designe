22dd2472e289a9a76c26df9038b6ba736b9d0153
md mohosin <mdmohosin.web@gmail.com>
2026-02-04 17:12:39 +0600

    add link in btn

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,8 @@ export default defineConfig({
         single: resolve(__dirname, 'productDetails.html'),
         shopping: resolve(__dirname, 'shoppingCart.html'),
         billing: resolve(__dirname, 'billingDetails.html'),
+        blogs: resolve(__dirname, 'blogs.html'),
+        blogDetails: resolve(__dirname, 'blogDetails.html'),
       },
     },
   },

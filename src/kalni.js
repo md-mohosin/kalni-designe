a9519dd30a43df43e9d05e93dcc9c784611b6408
md mohosin <mdmohosin.web@gmail.com>
2026-02-13 23:07:47 +0600

    add nav link

--- src/kalni.js
+++ src/kalni.js
@@ -6,5 +6,12 @@ const sidebar = document.getElementById("sidebar")
 menuIcon.addEventListener("click",()=>{
     menuIcon.classList.toggle("active")
     sidebar.classList.toggle("active")
+    if(sidebar.classList.contains("active")){
+        document.body.style.overflow = 'hidden'
+    }
+    else{
+        document.body.style.overflow = 'auto'
+    }
 })
+
 

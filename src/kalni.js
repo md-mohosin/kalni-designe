eca63f0d8a993466a4c56fa2a9ff323395a12374
md mohosin <mdmohosin.web@gmail.com>
2026-02-15 23:44:30 +0600

    designe card slider

--- src/kalni.js
+++ src/kalni.js
@@ -3,15 +3,13 @@ const sidebar = document.getElementById("sidebar")
 
 
 
-menuIcon.addEventListener("click",()=>{
+menuIcon.addEventListener("click", () => {
     menuIcon.classList.toggle("active")
     sidebar.classList.toggle("active")
-    if(sidebar.classList.contains("active")){
+    if (sidebar.classList.contains("active")) {
         document.body.style.overflow = 'hidden'
     }
-    else{
+    else {
         document.body.style.overflow = 'auto'
     }
 })
-
-

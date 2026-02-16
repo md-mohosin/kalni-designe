63d519e82af7345d4d27a253f7a83a59507e5f93
md mohosin <mdmohosin.web@gmail.com>
2026-02-16 19:35:51 +0600

    create slider btn

--- src/kalni.js
+++ src/kalni.js
@@ -24,10 +24,35 @@ menuIcon.addEventListener("click", () => {
 const carousel = document.querySelector(".carousel")
 const arrowBtns = document.querySelectorAll(".wrapper i")
 const firstCardWidth = carousel.querySelector(".card").offsetWidth
+const maxScrollLeft = carousel.scrollWidth - carousel.clientWidth
+
+
+function updateArrowColor() {
+    if (carousel.scrollLeft <= 0) {
+        document.getElementById("left").style.background = 'white'
+    }
+    else {
+        document.getElementById("left").style.background = "linear-gradient(to top, #44bde2, #088aed)"
+    }
+
+    if (carousel.scrollLeft >= maxScrollLeft) {
+        document.getElementById("right").style.background = 'white'
+    }
+    else {
+        document.getElementById("right").style.background = "linear-gradient(to top, #44bde2, #088aed)"
+    }
+
+}
+
+
+
 
 arrowBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         // **This is the main line of code that creates a slider.**
         carousel.scrollLeft += btn.id === 'left' ? -firstCardWidth : firstCardWidth
+        setTimeout(updateArrowColor, 100)
     })
 })
+
+updateArrowColor()
